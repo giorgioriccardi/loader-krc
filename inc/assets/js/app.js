ef347b4423c99e35012b9d0edf95a6fa264d3a55
Giorgio Riccardi <griccardi@my.bcit.ca>
2016-02-09 11:07:47 -0800

    loader 2016

---
+++ inc/assets/js/app.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function ($) {
+    // "use strict";
+
+    /* --------------------------------
+     Preloader
+     -------------------------------- */
+
+    $(window).load(function () {
+        // $('#preloader').fadeOut('slow', function () {
+        //     $(this).remove();
+        // });
+    	$('#preloader').fadeOut('slow');
+    });
+});
